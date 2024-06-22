2dc3433c2cb571c79646e0ab0aa2459a6493b30e
realbugdigger <marko@bugdigger.com>
2024-06-23 00:06:32 +0200

    making LERP and bazier curve finally!!!
    checkout LERP for short distances and stutters for aimbot

--- csgoHack/src/core/hack.h
+++ csgoHack/src/core/hack.h
@@ -119,21 +119,44 @@ private:
 		std::cout << "\t\t[***] Pitch: " << pitch << "\n";
 		std::cout << "\t\t[***] Yaw: " << yaw << "\n";
 
-		/*std::mt19937 gen{ std::random_device{}() };
+		std::mt19937 gen{ std::random_device{}() };
 		std::uniform_int_distribution<int> dist{ -4, 4 };
 		int random_number = 0;
 		do {
 			random_number = dist(gen);
-		} while (random_number == 0);*/
+		} while (random_number == 0);
 
-		if (pitch >= -89 && pitch <= 89 && yaw >= -180 && yaw <= 180)
+		if (pitch >= -25 && pitch <= 25 && yaw >= -40 && yaw <= 40) {
+			std::cout << "\t\tLERP !!!!!\n";
+
+			constexpr int steps{ 50 }; // Number of steps for interpolation
+			constexpr int delayMs{ 10 }; // Delay in milliseconds between steps
+
+			for (int i{ 0 }; i <= steps; i++)
+			{
+				// 't' represents the interpolation parameter ranging from 0 to 1.
+				// It indicates the relative position between the start and end points:
+				// - At t = 0, the position is at the start.
+				// - At t = 1, the position is at the end.
+				// - At 0 < t < 1, the position is proportionally between the start and end.
+				const float t{ static_cast<float>(i) / static_cast<float>(steps) };
+				
+				const float newPitch{ viewAngles->x + static_cast<float>((pitch - viewAngles->x) * t) };
+				const float newYaw{ viewAngles->y + static_cast<float>((yaw - viewAngles->y) * t) };
+				viewAngles->x = newPitch;
+				viewAngles->y = newYaw;
+
+				Sleep(delayMs);
+			}
+		}
+		else if (pitch >= -89 && pitch <= 89 && yaw >= -180 && yaw <= 180)
 		{
 			constexpr int steps{ 50 }; // Number of steps for interpolation
-			constexpr int delayMs{ 3 }; // Delay in milliseconds between steps
+			constexpr int delayMs{ 10 }; // Delay in milliseconds between steps
 
-			/*const POINT startingPoint{viewAngles->x, viewAngles->y};
+			const POINT startingPoint{viewAngles->x, viewAngles->y};
 			const POINT destinationPoint{ pitch, yaw };
-			const POINT midPoint{ startingPoint.x + (destinationPoint.x - startingPoint.x) / abs(random_number), destinationPoint.y + 3 * random_number };*/
+			const POINT midPoint{ startingPoint.x + (destinationPoint.x - startingPoint.x) / abs(random_number), destinationPoint.y + 3 * random_number };
 			
 			for (int i{ 0 }; i <= steps /*&& enemyInSight.load()*/; i++)
 			{
@@ -143,20 +166,13 @@ private:
 				// - At t = 1, the position is at the end.
 				// - At 0 < t < 1, the position is proportionally between the start and end.
 				float t{ static_cast<float>(i) / static_cast<float>(steps) };
-				//float t = i * 0.01;
+				
+				const POINT bezierPoint{ CalculateBezierPoint(t, startingPoint, midPoint, destinationPoint) };
 
-				//const POINT bezierPoint{ CalculateBezierPoint(t, startingPoint, midPoint, destinationPoint) };
-
-				const float newPitch{ viewAngles->x + static_cast<float>((pitch - viewAngles->x) * t) };
-				const float newYaw{ viewAngles->y + static_cast<float>((yaw - viewAngles->y) * t) };
-				viewAngles->x = newPitch;
-				viewAngles->y = newYaw;
-
-				//viewAngles->x = bezierPoint.x;
-				//viewAngles->y = bezierPoint.y;
-
+				viewAngles->x = bezierPoint.x;
+				viewAngles->y = bezierPoint.y;
+				
 				Sleep(delayMs);
-				//std::this_thread::sleep_for(std::chrono::milliseconds(delayMs));
 			}
 
 			/*viewAngles->x = pitch;
